436939d391376e7278923313ac1aa50bd2b03b90
MarkusWikman <Wikman.markus@gmail.com>
2024-02-13 11:27:02 -0600

    Senaste ändringarna 2024-02-13

--- eShop.UI/Classes/ProductCard.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace eShop.UI.Classes
-{
-    public class ProductCard
-    {
-        public string Image { get; set; }
-        public string Heading { get; set; }
-    }
-}
---
+++ eShop.UI/Services/UIService.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using eShop.API.DTO;
+using eShop.API.DTO.DTOs;
+using eShop.UI.Http.Clients;
+using eShop.UI.Models.Link;
+
+namespace eShop.UI.Services;
+public class UIService(CategoryHttpClient categoryHttp)
+{
+    List<CategoryGetDTO> Categories { get; set; } = [];
+    public List<ProductGetDTO> Products { get; private set; } = [];
+    public List<LinkGroup> CategoryLinkGroups { get; private set; } =
+    [
+        new LinkGroup
+        {
+            Name = "Categories"
+            ,
+            LinkOptions = new(){
+                new LinkOption { Id = 1, Name = "Women", IsSelected = true },
+                new LinkOption { Id = 2, Name = "Men", IsSelected = false },
+                new LinkOption { Id = 3, Name = "Children", IsSelected = false }
+            }
+        }
+    ];
+    public int CurrentCategoryId { get; set; }
+    //public async Task GetLinkGroup()
+    //{
+    //    Categories = await categoryHttp.GetCategoriesAsync();
+    //    CategoryLinkGroups[0].LinkOptions = mapper.Map<List<LinkOption>>(Categories);
+    //    var linkOption = CaregoryLinkGroups[0].LinkOptions.FirstOrDefault();
+    //    linkOption!.IsSelected = true;
+    //}
+    //public async Task OnCategoryLinkClick(int id)
+    //{
+    //    CurrentCategoryId = id;
+    //    await GetProductsAsync();
+    //    CategoryLinkGroups[0].LinkOptions.ForEach(l => l.IsSelected = false);
+    //    CategoryLinkGroups[0].LinkOptions.Single(l => l.Id.Equals(CurrentCategoryId)).IsSelected = true;
+    //}
+
+    //public async Task GetProductsAsync() =>
+    //    Products = await productHttp.GetProductsAsync(CurrentCategoryId);
+
+}
+
